02054d70b0b76386093e9883e0775f0d34c0680c
Raynnon <assante.f@gmail.com>
2021-07-13 21:53:12 +0200

    began to add react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Home from "./components/home/Home";
 import Category from "./components/category/Category";
 import Product from "./components/product/Product";
-import Container from "react-bootstrap/Container";
 
 import "./App.scss";
 
 function App() {
   return (
-    <Container className="App" fluid>
-      <Category />
-      {/* <Home /> */}
-      {/* <Product /> */}
-    </Container>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">About</Link>
+          </li>
+          <li>
+            <Link to="/product/5">Dashboard</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/category">
+            <Category />
+          </Route>
+          <Route path="/product/:id">
+            <Product />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
