e56b6689fec66dd4c5ce01d892e472a4a00cf789
Raynnon <assante.f@gmail.com>
2021-07-13 21:02:13 +0200

    finished category page

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -25,8 +25,9 @@ function Category() {
       <Header />
       <main className="mb-5">
         <h1 className="text-center mb-5">Cadeaux pour papa</h1>
-        <Row>
+        <Row id="description" className="ml-0">
           <Col
+            id="category-image"
             xs={5}
             className="bg-light"
             style={{
@@ -36,11 +37,10 @@ function Category() {
               backgroundAttachment: "scroll",
               backgroundPosition: "center center",
               position: "relative",
-              zIndex: 100,
               height: "25vh",
             }}
           ></Col>
-          <Col xs={7} className="d-flex align-items-center">
+          <Col className="d-flex align-items-center">
             <p className="text-justify mb-0 mx-5">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
               ullamcorper pharetra sagittis. Curabitur a sem eget dui iaculis
@@ -55,92 +55,95 @@ function Category() {
           </Col>
         </Row>
 
-        <div>
-          <Form className="bg-light d-flex align-items-center justify-content-between px-5 mb-5 mt-5">
-            <div>
-              <select className="custom-select mr-5" style={{ width: "200px" }}>
+        <Row
+          id="top-filters"
+          className="bg-light d-flex align-items-center justify-content-between px-5 py-2 mb-5 mt-5 ml-0 w-100"
+        >
+          <Form>
+            <div id="top-select">
+              <select className="custom-select mr-5" style={{ width: "150px" }}>
                 <option selected>-- Trier par</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
               </select>
-              <select className="custom-select" style={{ width: "200px" }}>
-                <option selected>-- Produit par page</option>
+              <select className="custom-select" style={{ width: "190px" }}>
+                <option selected>-- Produits par page</option>
                 <option value="1">12</option>
                 <option value="2">20</option>
                 <option value="3">32</option>
               </select>
             </div>
-
-            <p className="mb-0">
-              &lt; <span className="bg-white px-2 border border-muted">1</span>{" "}
-              ... <span>12</span> &gt;
-            </p>
           </Form>
-          <Row className="ml-0">
-            <Col xs={3} className="bg-light">
-              <Row>
-                <Col>
-                  <h4>Genre</h4>
-                  <ul>
-                    <li>Femme</li>
-                    <li>Homme</li>
-                    <li>Fille</li>
-                    <li>Garçon</li>
-                    <li>Bébé</li>
-                    <li>Animal</li>
-                    <li>Peu importe</li>
-                  </ul>
-                </Col>
-                <Col>
-                  <h4>Type</h4>
-                  <ul>
-                    <li>Maman</li>
-                    <li>Soeur</li>
-                    <li>Petit(e) copain / copine</li>
-                    <li>Collègue</li>
-                    <li>Ami / Amie</li>
-                    <li>Peu importe</li>
-                  </ul>
-                </Col>
-              </Row>
-            </Col>
-            <Col xs={9}>
-              <Row>
-                {products.map((product) => {
-                  return (
-                    <Col
-                      xs={6}
-                      md={6}
-                      lg={4}
-                      xl={3}
-                      className="d-flex justify-content-center text-center mb-4"
-                    >
-                      <div
-                        className="bg-light px-2 py-4"
-                        style={{ width: "100%" }}
-                      >
-                        <h3>{product.name}</h3>
-                        <Image
-                          src={product.imageSRC}
-                          alt="chaussures"
-                          width="90%"
-                        />
+          <p id="paging" className="mb-0">
+            &lt; <span className="bg-white px-2 border border-muted">1</span>{" "}
+            ... <span>12</span> &gt;
+          </p>
+        </Row>
 
-                        <Button
-                          className="bg-warning mt-3 border-0"
-                          style={{ borderRadius: "50px", width: "90%" }}
-                        >
-                          En savoir plus
-                        </Button>
-                      </div>
-                    </Col>
-                  );
-                })}
-              </Row>
-            </Col>
-          </Row>
-        </div>
+        <Row className="ml-0 justify-content-center">
+          <Col id="left-filters" xs={3} className="bg-light">
+            <Row>
+              <Col>
+                <h4>Genre</h4>
+                <ul>
+                  <li>Femme</li>
+                  <li>Homme</li>
+                  <li>Fille</li>
+                  <li>Garçon</li>
+                  <li>Bébé</li>
+                  <li>Animal</li>
+                  <li>Peu importe</li>
+                </ul>
+              </Col>
+              <Col>
+                <h4>Type</h4>
+                <ul>
+                  <li>Maman</li>
+                  <li>Soeur</li>
+                  <li>Petit(e) copain / copine</li>
+                  <li>Collègue</li>
+                  <li>Ami / Amie</li>
+                  <li>Peu importe</li>
+                </ul>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={9}>
+            <Row>
+              {products.map((product) => {
+                return (
+                  <Col
+                    xs={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    className="d-flex justify-content-center text-center mb-4"
+                  >
+                    <div
+                      className="bg-light px-2 py-4"
+                      style={{ width: "100%" }}
+                    >
+                      <h3>{product.name}</h3>
+                      <Image
+                        src={product.imageSRC}
+                        alt="chaussures"
+                        width="90%"
+                      />
+
+                      <Button
+                        className="bg-warning mt-3 border-0"
+                        style={{ borderRadius: "50px", width: "90%" }}
+                      >
+                        En savoir plus
+                      </Button>
+                    </div>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Col>
+        </Row>
       </main>
       <Footer />
     </div>
