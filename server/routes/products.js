3f106f428f780f90b95513be41be8a5cd531c2e8
Raynnon <assante.f@gmail.com>
2021-07-17 14:42:39 +0200

    finished add product route

--- server/routes/products.js
+++ server/routes/products.js
@@ -15,6 +15,17 @@ router.get("/products", async (req, res) => {
   }
 });
 
+/* READ PRODUCTS BY ID */
+router.get("/product/:id", async (req, res) => {
+  try {
+    //const products = await db.select().from("products");
+    res.send(req.params);
+  } catch (e) {
+    console.log(e);
+    res.status(200).send("Server unavailable");
+  }
+});
+
 /* ADD PRODUCT */
 const tempFolder = "../images/products/temp";
 
@@ -35,7 +46,9 @@ const upload = multer({ storage: storage });
 
 router.post("/addProduct", upload.array("image"), async (req, res) => {
   try {
-    const imagesFolder = "../images/products/" + req.body.name;
+    const imagesFolder =
+      "../images/products/" +
+      req.body.name.toLowerCase().trim().split(/\s+/).join("-");
 
     //Copy temp folder to the image folder
     fs.copy(tempFolder, imagesFolder, () => {
@@ -46,11 +59,36 @@ router.post("/addProduct", upload.array("image"), async (req, res) => {
       });
     });
 
-    console.log(req.files, req.body);
-    res.send(req.body);
+    const {
+      name,
+      price,
+      description,
+      strong_points,
+      who_kind,
+      who_type,
+      occasions,
+      parties,
+      url_amazon,
+      url_ali,
+    } = req.body;
+
+    await db("products").insert({
+      name,
+      price,
+      description,
+      strong_points,
+      who_kind,
+      who_type,
+      occasions,
+      parties,
+      url_amazon,
+      url_ali,
+      images_folder: imagesFolder,
+    });
+
+    res.send("Product added");
   } catch (e) {
-    console.log(e);
-    res.send(e);
+    res.status(400).send(e);
   }
 });
 
