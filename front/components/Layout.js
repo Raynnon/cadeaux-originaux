2cb3d8556386d92d0a478bdd7cf32ece07ab2726
Raynnon <assante.f@gmail.com>
2021-11-12 12:57:32 +0100

    added gtag for google analytics config

--- front/components/Layout.js
+++ front/components/Layout.js
@@ -33,28 +33,32 @@ export default function Layout({ children, pageTitle, description }) {
   return (
     <div className="text-coolGray-900">
       <Head>
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-            });
-          `
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`
           }}
-        />
+        ></script>
         <title>{pageTitle}</title>
         <meta name="description" content={description} />
       </Head>
       <Header categories={categories} />
-      <main>{children}</main>
+      <main>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </main>
       <Footer categories={categories} prices={prices} />
     </div>
   );
