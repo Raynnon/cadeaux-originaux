11577aa72f6f71671125a3289a2e5e2b7b0a299f
Raynnon <assante.f@gmail.com>
2022-02-05 20:31:46 +0100

    updated deprecated method

--- api/routes/crud/deleteItem.js
+++ api/routes/crud/deleteItem.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const deleteItem = async (_id, model) => {
   const data = await model.findOne({ _id });
 
-  fs.rmdirSync("./public/" + data.imagesFolder, { recursive: true });
+  fs.rmSync("./public/" + data.imagesFolder, { recursive: true });
   await model.deleteOne({ _id });
 };
 
