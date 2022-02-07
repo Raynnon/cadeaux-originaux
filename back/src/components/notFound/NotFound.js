cdaf9c409a5f8bb50543e6e37dfbba6bc07421a5
Raynnon <assante.f@gmail.com>
2022-02-07 19:25:26 +0100

    realigned items in 404 page

--- back/src/components/notFound/NotFound.js
+++ back/src/components/notFound/NotFound.js
@@ -29,52 +29,47 @@ const NotFound = () => {
         <source src={fireVideo} type="video/mp4" />
       </video>
 
-      <Box
-        component={"main"}
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center"
-        }}
+      <Grid
+        container
+        alignItems="center"
+        align="center"
+        sx={{ height: "100%" }}
       >
-        <Grid container alignItems="center" align="center" spacing={{ xs: 10 }}>
-          <Grid item xs={12} lg={4}>
-            <Typography
-              className="text-error"
-              variant="h1"
-              style={{ fontSize: "10vw", fontWeight: "bold" }}
-            >
-              Erreur
-            </Typography>
-          </Grid>
-          <Grid item xs={12} lg={5}>
-            <img id="santa-claus" src={santaImage} alt="Pere-noel-etonne" />
-          </Grid>
-          <Grid item xs={12} lg={3}>
-            <Typography
-              variant="h1"
-              style={{ fontSize: "10vw", fontWeight: "bold" }}
-            >
-              404
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Link to="/">
-              <Button
-                variant="contained"
-                size="large"
-                sx={{
-                  minWidth: "200px",
-                  fontSize: "calc(11px + 0.7vw)"
-                }}
-              >
-                Retour au site
-              </Button>
-            </Link>
-          </Grid>
+        <Grid item xs={12} lg={4}>
+          <Typography
+            className="text-error"
+            variant="h1"
+            style={{ fontSize: "10vw", fontWeight: "bold" }}
+          >
+            Erreur
+          </Typography>
         </Grid>
-      </Box>
+        <Grid item xs={12} lg={4}>
+          <img id="santa-claus" src={santaImage} alt="Pere-noel-etonne" />
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Typography
+            variant="h1"
+            style={{ fontSize: "10vw", fontWeight: "bold" }}
+          >
+            404
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Link to="/">
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                minWidth: "200px",
+                fontSize: "calc(11px + 0.7vw)"
+              }}
+            >
+              Retour au site
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
