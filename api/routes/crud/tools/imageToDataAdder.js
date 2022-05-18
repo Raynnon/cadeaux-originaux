793fd1a82d2e3b6f863e4ca6a302c0f6e2328127
Raynnon <assante.f@gmail.com>
2022-05-18 08:19:36 +0200

    continued to remake the header

--- api/routes/crud/tools/imageToDataAdder.js
+++ api/routes/crud/tools/imageToDataAdder.js
@@ -1,15 +1,14 @@
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 
 const imageToDataAdder = async (data) => {
   const dataToSend = data.map(async (item) => {
-    filesDirectory = "./public/" + item.imagesFolder;
+    filesDirectory = './public/' + item.imagesFolder;
 
     if (fs.existsSync(filesDirectory)) {
       const files = await fs.readdir(filesDirectory);
       const images = files.map((file) => {
-        return process.env.APP_URL + item.imagesFolder + "/" + file;
+        return process.env.APP_URL + item.imagesFolder + '/' + file;
       });
-
       const updatedItem = Object.assign(item, { images });
       return updatedItem;
     } else {
--- api/scripts/getProductionImages.js
+++ api/scripts/getProductionImages.js
@@ -1,8 +1,8 @@
-const axios = require("axios");
-const download = require("image-downloader");
+const axios = require('axios');
+const download = require('image-downloader');
 
-const dotenv = require("dotenv");
-const fs = require("fs");
+const dotenv = require('dotenv');
+const fs = require('fs');
 
 dotenv.config();
 
@@ -13,18 +13,20 @@ const getProductionImages = async (imagesRoute) => {
     const dir = product.imagesFolder;
 
     // If folder does not exists - Create folder then add pictures inside
-    if (!fs.existsSync("./public/" + dir)) {
+    if (!fs.existsSync('./public/' + dir)) {
       try {
-        fs.mkdirSync("./public/" + dir);
+        fs.mkdirSync('./public/' + dir);
+
         product.images.forEach(async (imageUrl) => {
           const options = {
             url: imageUrl,
-            dest: "./public/" + dir
+            dest: './public/' + dir
           };
+
           await download.image(options);
         });
       } catch (e) {
-        console.log("Images download failed");
+        console.log('Images download failed', e);
       }
     }
   });
