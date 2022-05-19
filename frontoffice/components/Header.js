e6ac7232431365fa89a0cc30ba744a852bf9a64d
Raynnon <assante.f@gmail.com>
2022-05-19 23:05:52 +0200

    finished subheader

--- frontoffice/components/Header.js
+++ frontoffice/components/Header.js
@@ -7,7 +7,7 @@ import logo from '../public/logos/logo-cadeaux-originaux-small.png';
 import logoWhite from '../public/logos/logo-cadeaux-originaux-blanc.png';
 
 export default function Header({ categories }) {
-  const [subHeaderLogoActive, setSubHeaderLogoActive] = useState(true);
+  const [subHeaderLogoActive, setSubHeaderLogoActive] = useState(false);
 
   if (typeof window !== 'undefined') {
     window.onscroll = () => {
@@ -19,8 +19,6 @@ export default function Header({ categories }) {
     };
   }
 
-  console.log(subHeaderLogoActive);
-
   return (
     <>
       <header className="items-center text-center h-14 flex justify-center">
@@ -31,24 +29,79 @@ export default function Header({ categories }) {
         </Link>
       </header>
 
-      <div className="hidden sticky top-0 bg-orange-400 py-1 z-50 px-16 lg:flex drop-shadow">
+      <div className="hidden sticky top-0 bg-orange-400 h-11 z-50 px-16 lg:flex drop-shadow">
         <Link href="/">
           <a
             className={`hidden ${
-              subHeaderLogoActive ? 'lg:hidden' : 'lg:block'
+              subHeaderLogoActive ? 'lg:inline' : 'lg:hidden'
             }`}
           >
-            <Image src={logoWhite} height={50} width={179} />
+            <Image src={logoWhite} height={35} width={125} />
           </a>
         </Link>
 
-        <nav className="flex justify-center flex-1 pr-52">
-          <ul className="flex flex-row justify-between align-center items-center">
+        <nav
+          className={`flex justify-center flex-1 pr-52 ${
+            !subHeaderLogoActive ? 'px-32' : 'px-0'
+          }`}
+        >
+          <ul className="h-full flex flex-row justify-between align-center items-center">
+            <li className="h-full mx-8 text-white font-semibold hover:underline hover:underline-offset-2 hover:cursor-pointer">
+              <Link
+                href={{
+                  pathname: '/category/Nouveau'
+                }}
+              >
+                <a className="h-full flex items-center">NOUVEAU</a>
+              </Link>
+            </li>
+            <li className="h-full mx-8 text-white font-semibold hover:underline hover:underline-offset-2 hover:cursor-pointer">
+              <Link
+                href={{
+                  pathname: '/category/Meilleurs-cadeaux'
+                }}
+              >
+                <a className="h-full flex items-center">MEILLEURES VENTES</a>
+              </Link>
+            </li>
             {Object.keys(categories).map((category, index) => {
               return (
-                <li className="mx-8 text-white font-semibold hover:underline hover:underline-offset-2 hover:cursor-pointer">
-                  {category.toUpperCase()}
-                </li>
+                <>
+                  <li
+                    className="h-full mx-8 text-white font-semibold hover:underline hover:underline-offset-2 hover:cursor-pointer group"
+                    key={index}
+                  >
+                    <p className="h-full flex items-center">
+                      {category.toUpperCase()}
+                    </p>
+
+                    <div className="group">
+                      <ul className="hidden absolute top-11 bg-white font-normal px-5 pt-0 pb-1 group-hover:block">
+                        {categories[category].map((subcategory, index) => {
+                          return (
+                            <li
+                              style={{ color: '#1F2937' }}
+                              className="py-1 my-1 border-b border-coolGray-200 last:border-0 last:my-0"
+                              key={index}
+                            >
+                              <Link
+                                href={{
+                                  pathname: `/category/${subcategory.name
+                                    .split(/[ ,]+/)
+                                    .join('-')}`
+                                }}
+                              >
+                                <a className="hover:text-orange-400">
+                                  {subcategory.name}
+                                </a>
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  </li>
+                </>
               );
             })}
           </ul>
